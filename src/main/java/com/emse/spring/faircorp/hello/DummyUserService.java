bf075efc73bedc669d959859563d1b2d18c0f9cb
Clément ROGER <clemmrogg@gmail.com>
2021-09-22 11:49:03 +0200

    working on greetAll method

---
+++ src/main/java/com/emse/spring/faircorp/hello/DummyUserService.java
@@ -0,0 +1,11 @@
+package com.emse.spring.faircorp.hello;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DummyUserService {
+
+    private GreetingService greetingService;
+
+
+}
